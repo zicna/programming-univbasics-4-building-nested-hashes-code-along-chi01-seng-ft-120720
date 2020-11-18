d814c64d4a99f1f114c93b6aa8100e14529aab18
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-18 20:28:52 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -2,14 +2,18 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch => ,
-      :matriarch => ,
-      :hero => ,
-      
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
       
       }, 
     :capulet => {
       #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       }}
 
   
