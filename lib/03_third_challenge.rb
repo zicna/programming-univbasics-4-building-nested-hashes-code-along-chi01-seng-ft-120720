eba2675cde396c6c36ce27b0d477ea64871039ab
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-18 20:33:26 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -8,22 +8,33 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        :name => 
+        :name =>  "Lady Montague",
+        :age => "54"
         },
       :hero => {
         #your key/value pairs here
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
         #your key/value pairs here
+        :name => "Lord Capulet",
+        :age => "50",
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
         #your key/value pairs here
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }
