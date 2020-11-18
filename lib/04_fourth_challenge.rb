280e9de90e7d3044a20b25bca1417396af79fdc2
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-18 20:39:38 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -7,10 +7,14 @@ def fourth_challenge
       :hero_friends => [
         #your hashes here!
         {
-          :name => 
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
         },
         {
-          
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
         }
       ]
    }, 
@@ -20,6 +24,15 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        },
+        {
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
       ]
    }
 }
