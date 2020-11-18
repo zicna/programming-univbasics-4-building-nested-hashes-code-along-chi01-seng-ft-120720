7525b72e4315536af7d1422c346a53648aca4b2e
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-18 20:35:31 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -4,16 +4,16 @@ def second_challenge
       #your key/value pairs here!
       :patriarch => {},
       :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
+      :hero => {},
+      :hero_friends => []
       
       }, 
     :capulet => {
       #your key/value pairs here!
       :patriarch => {},
       :matriarch => {},
-      :hero => {},
-      :hero_friends => []
+      :heroine => {},
+      :heroine_friends => []
       }}
 
   
